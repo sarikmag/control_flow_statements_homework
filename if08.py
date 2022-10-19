c3d2a3866c2afdfedb7d898816438def3012054a
sarikmag <sardorbek.abdujalilov@mail.ru>
2022-10-19 11:25:00 +0500

    asd

--- if08.py
+++ if08.py
@@ -11,4 +11,21 @@ def main(a):
     Returns:
         string: the message to print
     """
-    return
+    if a/10<10 and a/10>0:
+        if a%2==0:
+            return 'two-digit even number'
+        return 'two-digit odd numer'
+    if a/100<10 and a/100>0:
+        if a%2==0:
+            return 'three-digit even number'
+        return 'three-digit odd number'
+    if a/10>-10 and a/10<0:
+        if a%2==0:
+            return 'two-digit even number'
+        return 'two-digit odd numer'
+    if a/100>-10 and a/100<0:
+        if a%2==0:
+            return 'three-digit even number'
+        return 'three-digit odd number'
+    
+print(main(57))
